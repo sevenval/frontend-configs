82ccee71c3319fa48ff82086366293b1ad955c58
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-09-29 00:22:29 +0200

    Support sass if else in one line

--- index.js
+++ index.js
@@ -4,11 +4,39 @@ module.exports = {
     "stylelint-scss"
   ],
   rules: {
+    "at-rule-empty-line-before": [
+      "always",
+      {
+        except: [
+          "blockless-after-same-name-blockless",
+          "first-nested"
+        ],
+        ignore: [
+          "after-comment"
+        ],
+        ignoreAtRules: [
+          "if",
+          "else"
+        ]
+      }
+    ],
+    "block-closing-brace-newline-after": [
+      "always",
+      {
+        ignoreAtRules: [
+          "if",
+          "else"
+        ]
+      }
+    ],
     "color-hex-case": null,
     "color-hex-length": null,
     "color-named": null,
+    "declaration-colon-newline-after": null,
     "declaration-empty-line-before": null,
+    "function-name-case": null,
     "number-leading-zero": null,
-    "shorthand-property-no-redundant-values": null
+    "selector-max-compound-selectors": null,
+    "shorthand-property-no-redundant-values": null,
   }
 }
