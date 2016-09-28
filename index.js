ae3c247998159be1cd43f3daf70b274964302515
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-09-28 21:44:15 +0200

    Add config + tests

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: "stylelint-config-standard",
+  plugins: [
+    "stylelint-scss"
+  ],
+  rules: {
+    "color-hex-case": null,
+    "color-hex-length": null,
+    "color-named": null,
+    "number-leading-zero": null,
+    "max-nesting-depth": 4,
+    "shorthand-property-no-redundant-values": null
+  }
+}
