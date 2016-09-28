72775be61618cfd61f8778c9c8bd78127c2fd1ed
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-09-28 21:57:12 +0200

    Add max-compound-selector rule

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = {
     "color-hex-length": null,
     "color-named": null,
     "number-leading-zero": null,
+    "selector-max-compound-selectors": 3,
     "max-nesting-depth": 4,
     "shorthand-property-no-redundant-values": null
   }
