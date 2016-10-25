10375f785ee0ece905b534dfe2628ba538a5b828
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-10-25 14:03:52 +0200

    Add more react linting rules

---
+++ __tests__/react.spec.js
@@ -0,0 +1,21 @@
+const t = require('chai').assert;
+const helpers = require('./helpers');
+
+const runLinter = helpers.runLinter;
+const getFixture = helpers.getFixture;
+
+describe('React', () => {
+  it('should mark valid code', () => {
+    const code = getFixture('react/valid.js');
+    const result = runLinter(code, 'react');
+
+    t.equal(result.errorCount, 0);
+  });
+
+  it('should mark invalid code', () => {
+    const code = getFixture('react/invalid.js');
+    const result = runLinter(code, 'react');
+
+    t.equal(result.errorCount, 1);
+  });
+});
--- rules/react.js
+++ rules/react.js
@@ -11,5 +11,17 @@ module.exports = {
   ],
   plugins: [
     "react"
-  ]
+  ],
+  rules: {
+    'react/prefer-es6-class': ['error', 'always'],
+    'react/prefer-stateless-function': 'error',
+    'react/self-closing-comp': 'error',
+    'react/no-unused-prop-types': ['warn', {
+      customValidators: [],
+      skipShapeProps: true,
+    }],
+    'react/style-prop-object': 'error',
+    'react/no-unescaped-entities': 'error',
+    'react/no-children-prop': 'error'
+  }
 };
