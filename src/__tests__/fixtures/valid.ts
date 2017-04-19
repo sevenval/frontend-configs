3eb5eae32ddcee10f7f64ca3511914a01a83c9c2
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-04-19 14:47:57 +0200

    Import linter preset

---
+++ src/__tests__/fixtures/valid.ts
@@ -0,0 +1,10 @@
+
+export default class Foo {
+  private _baz() {
+    return "nope";
+  }
+}
+
+export function yo() {
+  return 2;
+}
---
+++ src/__tests__/helpers.ts
@@ -0,0 +1,14 @@
+import { Linter, Configuration } from "tslint";
+import * as fs from "fs";
+
+export function run(filename: string, config: string) {
+  const fileContents = fs.readFileSync(filename, "utf8");
+  const linter = new Linter({
+    fix: false,
+    formatter: "json",
+  });
+
+  const configuration = Configuration.findConfiguration(config, filename).results;
+  linter.lint(filename, fileContents, configuration);
+  return linter.getResult();
+}
