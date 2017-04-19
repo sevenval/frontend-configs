eb1d1bc7a604ec58611950d13682ea5d02ca4e86
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-04-19 15:12:16 +0200

    Move index.json to root
    Otherwise TSLint doesn't find the preset

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -6,7 +6,7 @@ const fixture = (file: string) =>
   path.join(__dirname, "fixtures", file);
 
 describe("Default Preset", () => {
-  const config = path.join(__dirname, "..", "index.json");
+  const config = path.join(__dirname, "..", "..", "index.json");
   it("should fail", () => {
     const res = run(fixture("invalid.ts"), config);
     t.equal(res.errorCount, 3);
